a9d897be98fbfb5295a4fc621631e571608a8ad1
Roman Lopez <roman.lopez93@yahoo.com>
2019-06-15 15:33:51 -0500

    did display for current-forecast. need to fix icon placement.

--- src/app/reducers/data.ts
+++ src/app/reducers/data.ts
@@ -2,15 +2,15 @@ import { WeatherInformation } from '../models/weather-information'
 import { WEATHER_SEARCH, Actions } from '../actions/data'
 
 const InitialState: WeatherInformation = {
-    city: '',
+    city: 'City', // change all of these
     temperature_current:  -1,
     temperature_high:  -1,
     temperature_low:  -1,
     humidity:  -1,
     wind:  -1,
-    description_main:  '',
-    description_detailed:  '',
-    icon:  '',
+    description_main:  'Desc',
+    description_detailed:  'Detailed Dexc',
+    icon:  'https://openweathermap.org/img/w/01d.png',
 }
 
 export function weatherReducer(state: WeatherInformation = InitialState, action: Actions){
